200aa151f252ef674e10c6925974f9e7d81de4c7
Ace <kronicrespawn@gmail.com>
2023-04-26 09:37:57 -0500

    navbar layout created

---
+++ src/component/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+} from './Navbar/NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <Bars />
+                <NavMenu>
+                    <NavLink to='/' activeStyle>
+                        Home
+                    </NavLink>
+                    <NavLink to = '/about' activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to ='/shop' activeStyle>
+                        Shop
+                    </NavLink>
+                    <NavLink to= '/checkout' activeStyle>
+                        Checkout
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='signin'>
+                        Sign In
+                    </NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
