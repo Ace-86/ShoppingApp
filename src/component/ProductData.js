d14c92f04bbbfe90162b96b7b6491d8f4202438a
Ace <kronicrespawn@gmail.com>
2023-04-27 12:44:25 -0500

    product database created

---
+++ src/component/ProductData.js
@@ -0,0 +1,38 @@
+export const Product = [
+
+    {
+        keyId: 0,
+        name: "DRMTLGY: Microdermabrasion Scrub",
+        image: "images/microderm.jpg",
+        description: "Gentle, non-abrasive pumice granules buff away dead cells and debris from the skin's surface to reduce the appearance of fine lines, acne scarring, and hyperpigmentation. Enriched ingredients help to soften and smooth skin with every use.",
+        price: "$19.99"
+    },
+    {
+        keyId: 1,
+        name: "Jelly Mask",
+        image: "images/jellymask.jpg",
+        description: "Get spa-quality skincare at home! They can be applied to any face structure with 5 herb-infused plant extract jelly masks use for facials, daily skincare, or a relaxing beauty routine.",
+        price: "15.99"
+    },
+    {
+        keyId: 2,
+        name: "",
+        image: "",
+        description: "",
+        price: ""
+    },
+    {
+        keyId: 3, 
+        name: "",
+        image: "",
+        description: "",
+        price: ""
+    },
+    {
+        keyId: 4,
+        name: "",
+        image: "",
+        description: "",
+        price: ""
+    }
+]
